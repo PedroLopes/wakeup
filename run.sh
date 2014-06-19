de245c34bde4f68f188fc5d802c0d79d525545da
pedro.lopes <pedro.lopes@hpi.uni-potsdam.de>
2014-06-19 16:05:53 +0200

    added this to my external GitHub repo

---
+++ run.sh
@@ -0,0 +1,3 @@
+#crontab line goes as follows: 00 09 * * * open -a Terminal.app /Users/pedro/Dropbox/1.projects/9.other/art/digital/wakeup/run.sh
+cd /Users/pedro/Dropbox/1.projects/9.other/art/digital/wakeup
+python wakeup.py
